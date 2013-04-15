2bbe354f1bd0d42781fb9c34325ffa8b6c0d9d4e
Corentin Wallez <corentin@wallez.net>
2013-04-15 02:28:13 +0200

    Add the meaning (ugly copy paste)

--- simulation.js
+++ simulation.js
@@ -24,7 +24,7 @@ function makeSimulationData(nNodes, nReplication){
     }
 }
 
-function Simulation(data, options){
+function FastPropagationSimulation(data, options){
     this.mask = (1 << data.nReplication) - 1
 
     //Initializes the state of the network
@@ -62,7 +62,7 @@ function Simulation(data, options){
     }
 }
 
-Simulation.prototype = {
+FastPropagationSimulation.prototype = {
     run: function(){
         this.running = true
         this.round = 0
@@ -149,6 +149,116 @@ Simulation.prototype = {
         }
         this.result.completion[this.round] = completed
         if(completed == this.nNodes || this.round > 40){
+            this.running = false
+            this.result.runningTime = this.round + 1
+        }
+    }
+}
+
+//TODO:merge both simulation
+function SlowPropagationSimulation(data, options){
+    //Initializes the state of the network
+    this.state = new Array(data.nNodes)
+    for(var i = 0; i < data.nNodes; i++){
+        if(i < data.nNodes / 2){
+            this.state[i] = -1
+        }else{
+            this.state[i] = 1
+        }
+    }
+
+    this.rt = data.rt
+    this.neighs = data.neighbours
+    this.ids = data.ids
+    this.nReplication = data.nReplication
+    this.nNodes = data.nNodes
+
+    this.doClosePull = "closePull" in options
+    this.doClosePush = "closePush" in options
+    this.doRandomPull = "randomPull" in options
+    this.doRandomPush = "randomPush" in options
+    this.doRandomApproxPull = "randomApproxPull" in options
+    this.doRandomApproxPush = "randomApproxPush" in options
+
+    this.result = {
+        runningTime: 0,
+        completion: [],
+        nNodes: data.nNodes,
+        nReplication: data.nReplication,
+    }
+}
+
+SlowPropagationSimulation.prototype = {
+    run: function(){
+        this.running = true
+        this.round = 0
+
+        order = this.ids.map(function(id){return id.n})
+
+        this.checkCompletion()
+        while(this.running){
+
+            this.shuffle(order)
+
+            var state = []
+            for(var i in this.state){
+                state[i] = this.state[i]
+            }
+
+            for(var i = 0; i < order.length; i++){
+                var current = order[i]
+
+                var bias = 0
+
+                if(this.doClosePull){
+                    var n = this.neighs[current]
+                    for(var j = 0; j < n.length; j++){
+                        bias += this.state[n[j]]
+                    }
+                }
+                if(this.doRandomPull){
+                    var id = Id()
+                    Id_randomize(id)
+                    var c = RT_findClosest(this.rt, id, this.nReplication)
+                    for(var j = 0; j < c.length; j++){
+                        bias += this.state[c[j].n]
+                    }
+                }
+                if(this.doRandomApproxPull){
+                    var n = this.choice(this.neighs)
+                    for(var j = 0; j < n.length; j++){
+                        bias += this.state[n[j]]
+                    }
+                }
+
+                state[i] = bias > 0 ? 1: (bias < 0 ? -1 : this.state[i]);
+            }
+
+            this.state = state
+
+            this.round ++
+            this.checkCompletion()
+        }
+
+        return this.result
+    },
+
+    shuffle: function(o){
+        for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x){}
+        return o
+    },
+
+    choice: function(o){
+        return o[parseInt(Math.random() * o.length)]
+    },
+
+    checkCompletion: function(){
+        var completed = 0
+        for(var i in this.state){
+            completed += this.state[i]
+        }
+        this.result.completion[this.round] = Math.abs(completed)
+        if(Math.abs(completed) == this.nNodes || this.round > 40){
             this.running = false
             this.result.runningTime = this.round + 1
         }
